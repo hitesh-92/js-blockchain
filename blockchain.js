1905195db652e07168eab805d518ed8b930125c5
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-03-07 10:56:50 +0000

    chain validation

--- blockchain.js
+++ blockchain.js
@@ -16,6 +16,25 @@ class Blockchain{
         return block
     }
 
+    isValidChain(chain){
+        //first block equals genesis block
+        const sameGenesis = JSON.stringify(chain[0]) == JSON.stringify(Block.genesis())
+        if( sameGenesis == false ) return false
+
+        //valid every block thereafter
+        for(let i=1; i<chain.length; i++){
+            const block = chain[i]
+            const lastBlock = chain[i-1]
+
+            const noMatch = block.lastHash !== lastBlock.hash
+            const badHash = block.hash !== Block.blockHash(block)
+            if(noMatch || badHash) return false
+            
+            return true
+        }
+        
+    }
+
 }
 
 module.exports = Blockchain
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Blockchain = require('./blockchain')
 const Block = require('./block')
 
 describe("Blockchain", () => {
-    let blockchain;
+    let blockchain, blockchain2;
 
     beforeEach(() => {
         blockchain = new Blockchain()
+        blockchain2 = new Blockchain()
     })
 
     it("starts with genesis block", () => {
@@ -19,8 +20,27 @@ describe("Blockchain", () => {
 
         const lastBlockPosition = blockchain.chain.length - 1
         const lastBlockData = blockchain.chain[lastBlockPosition].data
-        
+
         expect(lastBlockData).toEqual(data)
+    })
+
+    it("validates valid chain", () => {
+        blockchain2.addBlock('foo')
+
+        expect( blockchain.isValidChain( blockchain2.chain ) ).toBe(true)
+    })
+
+    it("invalids chain with corrupt gensis block", () => {
+        blockchain2.chain[0].data = 'badData'
+
+        expect( blockchain.isValidChain( blockchain2.chain ) ).toBe(false)
+    })
+
+    it("invalidates a corrupt chain", () => {
+        blockchain2.addBlock('foo')
+        blockchain2.chain[1].data = 'notFoo'
+
+        expect( blockchain.isValidChain( blockchain2.chain ) ).toBe(false)
     })
 
 })
