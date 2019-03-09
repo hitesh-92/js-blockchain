664b715df4ea597495ea5a52b082b39247afffca
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-03-09 17:31:39 +0000

    tests for transactionPool validTransaction method

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -9,9 +9,10 @@ describe("TransactionPool", () => {
     beforeEach(() => {
         tP = new TransactionPool()
         wallet = new Wallet()
-        transaction = Transaction.newTransaction(wallet, 'random-address', 30)
 
-        tP.updateOrAddTransaction(transaction)
+        // transaction = Transaction.newTransaction(wallet, 'random-address', 30)
+        // tP.updateOrAddTransaction(transaction)
+        transaction = wallet.createTransaction('random-address', 30, tP)
     })
 
     it("adds transaction to pool", () => {
@@ -29,6 +30,31 @@ describe("TransactionPool", () => {
         const updatedTransaction = tP.transactions.find(t => t.id === newTransaction.id)
 
         expect(JSON.stringify(updatedTransaction)).not.toEqual(oldTransaction)
+    })
+
+    describe("mixing valid and corrupt transactions", () => {
+
+        let validTransactions
+
+        beforeEach(() => {
+            validTransactions = [...tP.transactions]
+
+            for(let i=0; i<6; i++){
+                wallet = new Wallet()
+                transaction = wallet.createTransaction(`random-address`, 25, tP)
+                if(i%2 == 0) transaction.input.amount = 500000
+                else validTransactions.push(transaction)
+            }
+        })
+
+        it("shows difference between valid and corrupt transaction", () => {
+            expect(JSON.stringify(tP.transactions)).not.toEqual(JSON.stringify(validTransactions))
+        })
+
+        it("grabs valid transaction", () => {
+            expect(tP.validTransactions()).toEqual(validTransactions)
+        })
+
     })
 
 })
