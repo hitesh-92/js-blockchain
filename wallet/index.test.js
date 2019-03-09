74216ad3c9f5d1415d81c9f0f47a44a14e4e1349
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-03-09 19:32:02 +0000

    test cases for balance calculation

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,15 +1,16 @@
 const Wallet = require('./index')
 const TransactionPool = require('./transaction-pool')
 const Blockchain = require('../blockchain')
+const {INITIAL_BALANCE} = require('../config')
 
 describe("Wallet", () => {
 
-    let wallet, tP, blockchian
+    let wallet, tP, blockchain
 
     beforeEach(() => {
         wallet = new Wallet()
         tP =  new TransactionPool()
-        blockchian = new Blockchain()
+        blockchain = new Blockchain()
     })
 
     describe("creating transaction", () => {
@@ -19,13 +20,13 @@ describe("Wallet", () => {
         beforeEach(() => {
             sendAmount = 50
             recipient = 'random-address'
-            transaction = wallet.createTransaction(recipient, sendAmount, blockchian, tP)
+            transaction = wallet.createTransaction(recipient, sendAmount, blockchain, tP)
         })
 
         describe("doin the same transaction", () => {
             
             beforeEach(() => {
-                wallet.createTransaction(recipient, sendAmount, blockchian, tP)
+                wallet.createTransaction(recipient, sendAmount, blockchain, tP)
             })
 
             it("doubles 'sendAmount' subtracted from wallet balance", () => {
@@ -38,6 +39,65 @@ describe("Wallet", () => {
                 const sendAmounts = transaction.outputs.filter(output => output.address === recipient).map(output => output.amount)
 
                 expect(sendAmounts).toEqual([sendAmount, sendAmount])
+            })
+
+        })
+
+    })
+
+    describe("calculating balance", () => {
+
+        let addBalance, repeatAdd, senderWallet
+
+        beforeEach(() => {
+            senderWallet = new Wallet()
+            addBalance = 50
+            repeatAdd = 4
+
+            for(let i=0; i<repeatAdd; i++) senderWallet.createTransaction(wallet.publicKey, addBalance, blockchain, tP)
+            blockchain.addBlock(tP.transactions)
+        })
+
+        it("calculate balance for blockchain transactions matching recipient", () => {
+
+            const walletBalance = wallet.calcualteBalance(blockchain)
+
+            expect(walletBalance).toEqual(INITIAL_BALANCE + (addBalance * repeatAdd) )
+        })
+
+        it("calculate balance for blockchain transactions matching sender", () => {
+            const senderWalletBalance = senderWallet.calcualteBalance(blockchain)
+
+            expect(senderWalletBalance).toEqual(INITIAL_BALANCE - (addBalance * repeatAdd) )
+        })
+
+        describe("recipient conducts transaction", () => {
+
+            let subtractBalance, recipientBalance
+
+            beforeEach(() => {
+                tP.clear()
+                subtractBalance = 100
+                recipientBalance = wallet.calcualteBalance(blockchain)
+                wallet.createTransaction(senderWallet.publicKey, subtractBalance, blockchain, tP)
+                blockchain.addBlock(tP.transactions)
+            })
+
+            describe("sender sends another transaction to recipient", () => {
+
+                beforeEach(() => {
+                    tP.clear()
+                    senderWallet.createTransaction(wallet.publicKey, addBalance, blockchain, tP)
+                    blockchain.addBlock(tP.transactions)
+                })
+
+                it("calculates recipient balance using most recent transaction", () => {
+
+                    const walletBalance =  wallet.calcualteBalance(blockchain)
+
+                    expect(walletBalance).toEqual(recipientBalance - subtractBalance + addBalance)
+                })
+
             })
 
         })
